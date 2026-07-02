dc8a0791e9f89ddaecc2f09c0a99abcf3eb6e473
julius <juliusnjenga20002@gmail.com>
2026-07-02 21:19:23 +0300

    added stylessheet for fees only

--- student-portal/src/App.jsx
+++ student-portal/src/App.jsx
@@ -2,6 +2,7 @@ import Navigation from './components/Navigation.jsx';
 import LoginForm from './components/LoginForm.jsx';
 import StudentCard from './components/StudentCard.jsx';
 import NotFoundPage from './components/NotFoundPage.jsx';
+import Fees from './components/Fees.jsx';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path ='/' element ={<LoginForm />} />
       <Route path = '/StudentCard' element ={<StudentCard />} />
+      <Route path ='/fees' element = {<Fees />}></Route>
       <Route path = '*' element = {<NotFoundPage />}/>
     </Routes>
 
---
+++ student-portal/src/components/Fees.jsx
@@ -0,0 +1,16 @@
+import './Feesstyle.css';
+function Fees(){
+    return(
+        <>
+        <div className = 'fees'>
+            <p>Total fees - </p>
+            <p>Fees paid - </p>
+            <p>print fee sturcture - </p>
+
+
+        </div>
+        </>
+    )
+}
+
+export default Fees;
