9ebf0b617b508c14f12fd14dd072cd6dcb3b9456
julius <juliusnjenga20002@gmail.com>
2026-07-07 20:59:57 +0300

    removed node_modules on git

---
+++ back-end/student-portal-server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const port = 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/LoginForm' , (req , res)=>{
+    res.json({message : 'data recieved'});
+    console.log(`we recieved ${req.body.admin} password ${req.body.password}`);
+
+})
+
+const server = app.listen(port , ()=>{
+    console.log('SERVER RUNNIG A PORT : '  ,port);
+})
+
+server.on('error' , (err)=>{
+    if(err.code === 'EADDRINUSE'){
+        console.error(`PORT ${port} IS ALREADY IN USE err.message`);
+    } else{
+        console.error('SERVER ERROR');
+    }
+})
--- student-portal/src/components/utilities/ProtectiveRoutes.jsx
+++ student-portal/src/components/utilities/ProtectiveRoutes.jsx
@@ -1,6 +1,6 @@
 import { Outlet , Navigate } from 'react-router-dom';
 const ProtectiveRoutes = ()=>{
-    const user = null;
+    const user = true;
     return user ? <Outlet /> : <Navigate to='/'/>
 }
 
